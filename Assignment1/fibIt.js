dcbb61d5955ec9b37f057175324e949bfc2e6952
BalazsBanfi <banfibalazs81@gmail.com>
2023-10-20 20:07:01 +0200

    starting files

---
+++ Assignment1/fibIt.js
@@ -0,0 +1,13 @@
+const fib = () => {
+    let arr = [];
+
+    
+    return arr;
+}
+
+
+console.log(fib(-3));
+console.log(fib(-0));
+console.log(fib(3));
+console.log(fib(8));
+console.log(fib(27));
