9f2ec42094e0884d94a68b3dc84a1173b84c61b9
BalazsBanfi <banfibalazs81@gmail.com>
2023-10-20 20:20:07 +0200

    fib iteration ready

--- Assignment1/fibIt.js
+++ Assignment1/fibIt.js
@@ -1,13 +1,19 @@
-const fib = () => {
-    let arr = [];
-
-    
-    return arr;
-}
-
+const fib = (num) => {
+  let arr = [0, 1];
+  if (num < 1) {
+    return "Please add a number greater then zero";
+  }
+  if (num === 1) {
+    return [0];
+  }
+  for (let i = 1; i < num; i++) {
+    arr.push(arr[i - 1] + arr[i]);
+  }
+  return arr;
+};
 
 console.log(fib(-3));
-console.log(fib(-0));
-console.log(fib(3));
-console.log(fib(8));
+console.log(fib(0));
+console.log(fib(1));
+console.log(fib(2));
 console.log(fib(27));
